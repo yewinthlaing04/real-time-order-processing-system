c00d06b2e139f9aae9168202a6e8f43f7296e466
yewinthlaing04 <yewinthlaing23@gmail.com>
2025-03-28 21:12:01 +0100

    add config-server

---
+++ services/config-server/config-server/src/test/java/com/ye/ecommerce/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ye.ecommerce.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
